e0309b5821e0a0bfa50c1eacbf0f39b5aba002ac
Hai Binh <133617225+nghbinh@users.noreply.github.com>
2023-05-17 16:05:59 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -233,100 +233,101 @@ const App = () => {
     };
 
     return (
-        <div className="container">
-            <div className="heading">
-                <h1 className="intro">Scrum Values</h1>
-                <p>
-                    Scrum is an empirical process, where decisions are based on
-                    observation, experience and experimentation. Scrum has three
-                    pillars: transparency, inspection and adaptation. This
-                    supports the concept of working iteratively. Think of
-                    Empiricism as working through small experiments, learning
-                    from that work and adapting both what you are doing and how
-                    you are doing it as needed.
-                </p>
-                <p>
-                    One critical Scrum Team characteristic that binds all of the
-                    elements together is Trust. If Trust is not present on a
-                    Scrum Team, there will likely be tension and bottlenecks in
-                    the way of getting work done. The Scrum Values are also
-                    critical for Scrum Teams to adhere to as they help to guide
-                    how you work and drive trust. The Scrum Values of Courage,
-                    Focus, Commitment, Respect, and Openness, are all important
-                    elements that Scrum Team members must consider when working
-                    together. The Scrum Values are particularly important in
-                    environments where experimentation is core to making
-                    progress.
-                </p>
-                <img
-                    src={require("./img/scrum2.png")}
-                    alt="Scrum2"
-                    id="img2"
-                ></img>
-                <p>
-                    Read more about scrum at{" "}
-                    <a
-                        href="https://www.scrum.org/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        https://www.scrum.org/
-                    </a>
-                </p>
-                <p>
-                    To visualize your Scrum Value, you can use this tool below.{" "}
-                </p>
-                <p>
-                    Answer 5 questions for each value by selecting yes or no,
-                    depending on your own opinion.
-                </p>
-            </div>
-            <h3 className="tutorial">
-                Answer 5 questions for each value by selecting yes or no. The
-                result will be generate automatically.
-            </h3>
+        <div className="container p-3">
             <div className="row">
-                <div className="col-6">
-                    <div className="ScrumValuesForm">
-                        {values.map((value, index) => (
-                            <div key={index}>
-                                <h3 className="questions">
-                                    {value.name} - point: {value.points}
-                                </h3>
-                                {value.questions.map((question, i) => (
-                                    <div className="row question" key={i}>
-                                        <div className="col-10">
-                                            {question.question}
-                                        </div>
-                                        <div className="col-2">
-                                            <select
-                                                onChange={(e) =>
-                                                    handleSelectChange(
-                                                        e.target.value,
-                                                        i,
-                                                        index
-                                                    )
-                                                }
-                                            >
-                                                {question.options.map(
-                                                    (option, j) => (
-                                                        <option
-                                                            key={j}
-                                                            value={option}
-                                                        >
-                                                            {option}
-                                                        </option>
-                                                    )
-                                                )}
-                                            </select>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
+                <div className="col-md-12">
+                    <h3 className="intro">Scrum Values</h3>
                 </div>
-                <div className="col-6">
+                <div className="col-md-12">
+                    <p>
+                        Scrum is an empirical process, where decisions are based
+                        on observation, experience and experimentation. Scrum
+                        has three pillars: transparency, inspection and
+                        adaptation. This supports the concept of working
+                        iteratively. Think of Empiricism as working through
+                        small experiments, learning from that work and adapting
+                        both what you are doing and how you are doing it as
+                        needed.
+                    </p>
+                    <p>
+                        One critical Scrum Team characteristic that binds all of
+                        the elements together is Trust. If Trust is not present
+                        on a Scrum Team, there will likely be tension and
+                        bottlenecks in the way of getting work done. The Scrum
+                        Values are also critical for Scrum Teams to adhere to as
+                        they help to guide how you work and drive trust. The
+                        Scrum Values of Courage, Focus, Commitment, Respect, and
+                        Openness, are all important elements that Scrum Team
+                        members must consider when working together. The Scrum
+                        Values are particularly important in environments where
+                        experimentation is core to making progress.
+                    </p>
+                </div>
+                <img src={require("./img/scrum2.png")} alt="Scrum2" id="img2" />
+                <div className="col-md-12">
+                    <p>
+                        Read more about scrum at{" "}
+                        <a
+                            href="https://www.scrum.org/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            https://www.scrum.org/
+                        </a>
+                        .
+                    </p>
+                    <p>
+                        To visualize your Scrum Value, you can use this tool
+                        below.
+                    </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <h3 className="tutorial">
+                        Answer 5 questions for each value by selecting yes or
+                        no. The result will be generate automatically.
+                    </h3>
+                </div>
+                <div className="col-md-12 border rounded-3 p-2 form">
+                    {values.map((value, index) => (
+                        <div className="p-2" key={index}>
+                            <h3 className="section">
+                                {value.name} - point: {value.points}
+                            </h3>
+                            {value.questions.map((question, i) => (
+                                <div className="row p-1 align-items-center" key={i}>
+                                    <div className="col-10 ">
+                                        {question.question}
+                                    </div>
+                                    <div className="col-2">
+                                        <select
+                                            onChange={(e) =>
+                                                handleSelectChange(
+                                                    e.target.value,
+                                                    i,
+                                                    index
+                                                )
+                                            }
+                                        >
+                                            {question.options.map(
+                                                (option, j) => (
+                                                    <option
+                                                        key={j}
+                                                        value={option}
+                                                    >
+                                                        {option}
+                                                    </option>
+                                                )
+                                            )}
+                                        </select>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+				<div className="col-md-12 chart">
                     <Radar data={data} options={options} />
                 </div>
             </div>
